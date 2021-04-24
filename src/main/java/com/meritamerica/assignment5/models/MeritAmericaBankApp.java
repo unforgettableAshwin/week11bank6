311527d650bde98c5e7eb7e562940df52e080390
ashwin nirmul da21-01-6311 <ashwin.metropcs@gmail.com>
2021-04-24 00:59:49 -0400

    Added the validation dependency and commented out the extra main().

--- src/main/java/com/meritamerica/assignment5/models/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment5/models/MeritAmericaBankApp.java
@@ -1,8 +1,10 @@
 package com.meritamerica.assignment5.models;
 
-public class MeritAmericaBankApp {
-	public static void main(String[] args) {
-		//MeritBank.readFromFile("src/test/testMeritBank_good.txt");
-		MeritBank.writeToFile("src/main/transactionHistory");
-	}
+public class MeritAmericaBankApp
+{
+//	public static void main( String[] args )
+//	{
+//		// MeritBank.readFromFile("src/test/testMeritBank_good.txt");
+//		MeritBank.writeToFile( "src/main/transactionHistory" );
+//	}
 }
